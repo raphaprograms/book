ab21726fd68782d6f6cbe9c4a583c21ea4a1f264
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-04-05 08:56:47 -0700

    Create funtions to add and display books
    Still need to style book display divs before next step

---
+++ script.js
@@ -0,0 +1,46 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+      }
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return `${this.title} by ${author}, ${pages} pages, ${read}.`;
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    return myLibrary;
+
+}
+
+/*addBookToLibrary('Harry Potter', 'J.K. Rowland', '500', 'has been read');
+console.log(myLibrary); */
+
+let container = document.querySelector('.container');
+
+function displayBook(title, author, pages, read) {
+    addBookToLibrary(title, author, pages, read);
+    let book = myLibrary[myLibrary.length - 1];
+
+    let bookCard = document.createElement('div');
+    bookCard.textContent = book.info(),
+    container.appendChild(bookCard);
+}
+
+displayBook('Harry Potter', 'J.K. Rowland', '500', 'has been read');
+console.log(myLibrary);
+
+
+/* const book1 = new Book('Harry Potter', 'J.K. Rowland', '500', 'has been read'); 
+
+console.log(book1.info());
+
+console.log(Object.getPrototypeOf(book1) === Book.prototype); */
