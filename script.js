b94e30add749a677b512ec89155cb523df5e73fc
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-04-15 11:11:51 -0700

    Add test logic for form and book card.
    Button to add book has dissapeared, need to debug

--- script.js
+++ script.js
@@ -15,25 +15,85 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    let newBook = new Book(title, author, pages, read);
+    const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
     return myLibrary;
 
 }
+const container = document.querySelector('.container');
 
-let container = document.querySelector('.container');
 let clicktoAddBtn = document.createElement('button')
 clicktoAddBtn.textContent = 'Click ME to Add a Book';
 container.appendChild(clicktoAddBtn);
 
+const form = document.createElement('form');
+form.style.display = 'none';
 
-function displayBook(title, author, pages, read) {
-    addBookToLibrary(title, author, pages, read);
+const titleInput = document.createElement('input');
+titleInput.type = 'text';
+titleInput.name = 'title';
+titleInput.placeholder = 'Title';
+titleInput.required = true;
+
+const addBookBtn = document.createElement('button');
+addBookBtn.type = 'submit';
+addBookBtn.textContent = 'Add Book';
+form.appendChild(addBookBtn);
+
+form.appendChild(titleInput);
+container.appendChild(form);
+
+clicktoAddBtn.addEventListener('click', () => {
+    form.style.display = 'flex';
+    form.style.flexWrap = 'wrap';
+    form.styly.gap = '0.5em';
+    clicktoAddBtn.style.display = 'none';
+});
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const title = titleInput.value;
+    const author = 'author';
+    const pages = 'pages';
+    const read = 'read';
+
+    const newBook = addBookToLibrary(title, author, pages, read);
+    displayBook(newBook);
+
+    form.reset();
+    form.style.display = 'none';
+    clicktoAddBtn.style.display = 'inline-block';
+});
+
+
+function displayBook(book) {
     let book = myLibrary[myLibrary.length - 1];
 
     let bookCard = document.createElement('div');
-    bookCard.textContent = book.info(),
     bookCard.classList.add('book-card');
+    bookCard.dataset.id = book.id;
+
+    const cardInfo = document.createElement('div');
+    cardInfo.textContent = book.info();
+
+    const toggleReadBtn = document.createElement('button');
+    toggleReadBtn.textContent = 'Read / Not Read';
+    toggleReadBtn.addEventListener('click', () => {
+        toggleReadBtn.textContent = 'hmmmm'
+        cardInfo.textContent = book.info();
+    });
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.addEventListener('click', () => {
+        bookCard.remove();
+        myLibrary = myLibrary.filter(b => b.id !== book.id);
+    })
+    bookCard.appendChild(cardInfo);
+    bookCard.appendChild(toggleReadBtn);
+    bookCard.appendChild(deleteBtn);
+
     container.appendChild(bookCard);
 }
 
